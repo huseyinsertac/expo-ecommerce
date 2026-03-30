5425f049a7099b1d6b6d201fd9e91fd570a8ea29
CypherPunk2071 <grg170dx@yahoo.com>
2026-03-30 08:58:14 +0300

    admin dashboard tools setup

---
+++ admin/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/clerk-react';
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <SignIn></SignIn>
+    </div>
+  );
+}
+
+export default LoginPage;
