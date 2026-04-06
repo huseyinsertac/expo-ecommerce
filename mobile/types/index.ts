ebed067ee0cb7c7da52b92048244053688edf632
CypherPunk2071 <grg170dx@yahoo.com>
2026-04-06 08:14:12 +0300

    mobile app setup with Clerk and React Query, along with some file restructuring and asset additions.

---
+++ mobile/types/index.ts
@@ -0,0 +1,98 @@
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+  images: { url: string; public_id: string }[];
+  averageRating: number;
+  totalReviews: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Address {
+  _id: string;
+  label: string;
+  fullName: string;
+  streetAddress: string;
+  street: string;
+  city: string;
+  stateCode: string;
+  zip: string;
+  phoneNumber: string;
+  country: string;
+  isDefault: boolean;
+}
+
+export interface User {
+  _id: string;
+  clerkId: string;
+  email: string;
+  name: string;
+  imageUrl: string;
+  addresses: Address[];
+  wishlist: string[] | Product[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface OrderItem {
+  _id: string;
+  productId: string | Product;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+export interface Order {
+  _id: string;
+  user: string;
+  clerkId: string;
+  orderItems: OrderItem[];
+  shippingAddress: {
+    label: string;
+    fullName: string;
+    streetAddress: string;
+    city: string;
+    state: string;
+    zip: string;
+    phoneNumber: string;
+  };
+  paymentResult: {
+    id: string;
+    status: string;
+  };
+  totalPrice: number;
+  status: "pending" | "shipped" | "delivered" | "cancelled";
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Review {
+  _id: string;
+  productId: string;
+  userId: string | User;
+  orderId: string;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CartItem {
+  _id: string;
+  productId: string | Product;
+  quantity: number;
+}
+
+export interface Cart {
+  _id: string;
+  userId: string;
+  clerkId: string;
+  items: CartItem[];
+  createdAt: string;
+  updatedAt: string;
+}
