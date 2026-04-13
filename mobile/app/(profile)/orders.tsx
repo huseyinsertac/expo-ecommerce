3a97664b0bd044e5b176b7c5333abf19abcffac3
CypherPunk2071 <grg170dx@yahoo.com>
2026-04-13 16:26:40 +0300

    wishlist screen added

---
+++ mobile/app/(profile)/orders.tsx
@@ -0,0 +1,15 @@
+import SafeScreen from '@/components/SafeScreen';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function OrdersScreen() {
+  return (
+    <SafeScreen>
+      <View>
+        <Text>OrdersScreen</Text>
+      </View>
+    </SafeScreen>
+  );
+}
+
+export default OrdersScreen;
